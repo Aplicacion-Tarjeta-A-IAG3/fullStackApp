52448fbcc93044ae2edda563e86cd28d7616aab5
Julia Espinoza <juespinozacorrea@gmail.com>
2020-07-20 02:27:48 -0300

    Connect create transacciones

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -5,8 +5,8 @@ import { Title } from "react-admin";
 export default ({ permissions }) => {
   const title = {
     admin: "Administrador",
-    cliente: "Administrador",
-    comercio: "Administrador",
+    persona: "Cliente",
+    comercio: "Comercio",
   };
 
   const description = {
