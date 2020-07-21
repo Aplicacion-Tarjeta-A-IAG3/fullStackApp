470da8b85fcfc0184c1696beae5b6391d4aa5242
Julia Espinoza <juespinozacorrea@gmail.com>
2020-07-21 00:17:41 -0300

    added show page and updated create form(mod)

--- src/components/transactions.js
+++ src/components/transactions.js
@@ -11,6 +11,8 @@ import {
   FunctionField,
   NumberField,
   DateField,
+  Show,
+  SimpleShowLayout,
 } from "react-admin";
 
 export const TransactionList = (props) => (
@@ -31,42 +33,56 @@ export const TransactionList = (props) => (
   </List>
 );
 
+export const TransactionShow = (props) => (
+  <Show {...props}>
+    <SimpleShowLayout>
+      <h5>Datos de la transacción</h5>
+      <NumberField label="Monto" source="monto" />
+      <NumberInput label="Cuotas" fullWidth source="cuotas" />
+      <DateField label="Fecha de transacción" source="fecha" />
+      <TextField label="Detalle" source="detalle" />
+      <h5>Datos del cliente</h5>
+      <NumberField label="DNI" source="persona.dni" />
+      <TextField label="Nombre" source="persona.nombre" />
+      <TextField label="Apellido" source="persona.apellido" />
+      <h5>Datos de la tarjeta</h5>
+      <NumberField label="Número" source="tarjeta.numero" />
+      <DateField label="Vencimiento" source="tarjeta.fechaVencimiento" />
+      <h5>Datos del comercio</h5>
+      <NumberField label="CUIT" source="comercio.cuit" />
+      <TextField label="Nombre" source="comercio.nombre" />
+    </SimpleShowLayout>
+  </Show>
+);
+
 export const TransactionCreate = (props) => (
   <Create title="Postnet virtual" {...props}>
     <SimpleForm>
-      <div>
-        <h5>Datos del comercio</h5>
-        <NumberInput fullWidth required label="CUIT" source="cuit" />
-        <TextInput fullWidth label="Nombre del comercio" source="comercio" />
-      </div>
-      <div>
-        <h5>Datos del cliente</h5>
-        <NumberInput fullWidth required label="DNI" source="dni" />
-        <TextInput fullWidth source="nombre" />
-        <TextInput fullWidth source="apellido" />
-      </div>
-      <div>
-        <h5>Datos de la tarjeta</h5>
-        <NumberInput
-          fullWidth
-          required
-          label="Número de tarjeta"
-          source="tarjeta"
-        />
-        <DateInput
-          fullWidth
-          required
-          label="Vencimiento"
-          source="vencimiento"
-          defaultValue={new Date()}
-        />
-        <NumberInput fullWidth required label="CVC" source="cvc" />
-      </div>
-      <div>
-        <h5>Datos de la venta</h5>
-        <NumberInput fullWidth required source="monto" />
-        <NumberInput fullWidth source="cuotas" />
-      </div>
+      <h5>Datos del comercio</h5>
+      <NumberInput fullWidth required label="CUIT" source="cuit" />
+      <TextInput fullWidth label="Nombre del comercio" source="comercio" />
+      <h5>Datos del cliente</h5>
+      <NumberInput fullWidth required label="DNI" source="dni" />
+      <TextInput fullWidth source="nombre" />
+      <TextInput fullWidth source="apellido" />
+      <h5>Datos de la tarjeta</h5>
+      <NumberInput
+        fullWidth
+        required
+        label="Número de tarjeta"
+        source="tarjeta"
+      />
+      <DateInput
+        fullWidth
+        required
+        label="Vencimiento"
+        source="vencimiento"
+        defaultValue={new Date()}
+      />
+      <NumberInput fullWidth required label="CVC" source="cvc" />
+      <h5>Datos de la venta</h5>
+      <NumberInput fullWidth required source="monto" />
+      <NumberInput fullWidth source="cuotas" />
     </SimpleForm>
   </Create>
 );
