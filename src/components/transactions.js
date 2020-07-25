f62acc04bbaad09b973e702319ff9aca173057b4
Julia Espinoza <juespinozacorrea@gmail.com>
2020-07-25 17:51:38 -0300

    posnet virtual con los datos del comercio cargado

--- src/components/transactions.js
+++ src/components/transactions.js
@@ -14,8 +14,16 @@ import {
   Show,
   SimpleShowLayout,
   downloadCSV,
+  Loading,
 } from "react-admin";
 import jsonExport from "jsonexport/dist";
+
+const apiUrl = "https://african-express.us-e2.cloudhub.io/api/core";
+const headers = {
+  "Content-Type": "application/json",
+  client_id: localStorage.getItem("clientId"),
+  client_secret: localStorage.getItem("clientSecret"),
+};
 
 const exporter = (transactions) => {
   const transactionsExport = transactions.map((transaction) => {
@@ -84,34 +92,74 @@ export const TransactionShow = (props) => (
   </Show>
 );
 
-export const TransactionCreate = (props) => (
-  <Create title="Postnet virtual" {...props}>
-    <SimpleForm>
-      <h5>Datos del comercio</h5>
-      <NumberInput fullWidth required label="CUIT" source="cuit" />
-      <TextInput fullWidth label="Nombre del comercio" source="comercio" />
-      <h5>Datos del cliente</h5>
-      <NumberInput fullWidth required label="DNI" source="dni" />
-      <TextInput fullWidth source="nombre" />
-      <TextInput fullWidth source="apellido" />
-      <h5>Datos de la tarjeta</h5>
-      <NumberInput
-        fullWidth
-        required
-        label="Número de tarjeta"
-        source="tarjeta"
-      />
-      <DateInput
-        fullWidth
-        required
-        label="Vencimiento"
-        source="vencimiento"
-        defaultValue={new Date()}
-      />
-      <NumberInput fullWidth required label="CVC" source="cvc" />
-      <h5>Datos de la venta</h5>
-      <NumberInput fullWidth required source="monto" />
-      <NumberInput fullWidth source="cuotas" />
-    </SimpleForm>
-  </Create>
-);
+export const TransactionCreate = (props) => {
+  const [defaultFormValue, setDefaultFormValue] = React.useState({});
+
+  React.useEffect(() => {
+    const username = localStorage.getItem("username");
+    const fillBusinessData = async () => {
+      const options = {
+        method: "GET",
+        headers: headers,
+      };
+
+      const response = await fetch(
+        `${apiUrl}/comercios?cuit=${username}`,
+        options
+      );
+      const data = await response.json();
+      // console.log("business data", data);
+      setDefaultFormValue({
+        comercio: data[0].nombre,
+        cuit: data[0].cuit,
+        cuotas: 1,
+      });
+    };
+
+    fillBusinessData();
+  }, []);
+
+  return (
+    <Create title="Postnet virtual" {...props}>
+      <SimpleForm initialValues={defaultFormValue}>
+        {localStorage.getItem("username") && (
+          <div style={{ width: "100%" }}>
+            <h5>Datos del cliente</h5>
+            <NumberInput fullWidth required label="DNI" source="dni" />
+            <TextInput fullWidth source="nombre" />
+            <TextInput fullWidth source="apellido" />
+            <h5>Datos de la tarjeta</h5>
+            <NumberInput
+              fullWidth
+              required
+              label="Número de tarjeta"
+              source="tarjeta"
+            />
+            <DateInput
+              fullWidth
+              required
+              label="Vencimiento"
+              source="vencimiento"
+              defaultValue={new Date()}
+            />
+            <NumberInput fullWidth required label="CVC" source="cvc" />
+            <h5>Datos de la venta</h5>
+            <NumberInput fullWidth required source="monto" />
+            <NumberInput fullWidth source="cuotas" />
+            <h5>Datos del comercio</h5>
+            <NumberInput fullWidth disabled label="CUIT" source="cuit" />
+            <TextInput
+              fullWidth
+              disabled
+              label="Nombre del comercio"
+              source="comercio"
+            />
+          </div>
+        )}
+        {!localStorage.getItem("username") && (
+          <Loading loadingPrimary="Si toma mucho tiempo, refresca la página." />
+        )}
+      </SimpleForm>
+    </Create>
+  );
+};
