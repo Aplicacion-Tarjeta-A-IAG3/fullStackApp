7e6f67d567d231dcf127001b103c74ad30c675de
Julia Espinoza <juespinozacorrea@gmail.com>
2020-07-30 04:10:47 -0300

    fix harcoded cuit

--- src/components/bussiness/businessDailyBalance.js
+++ src/components/bussiness/businessDailyBalance.js
@@ -62,7 +62,7 @@ export default function BusinessDailyBalance(props) {
         headers: headers,
       };
 
-      const url = `${apiUrl}/transacciones/diario?cuit=${9827352865}`;
+      const url = `${apiUrl}/transacciones/diario?cuit=${username}`;
 
       const result = await fetch(url, requestOptions);
       console.log("status", result.status);
