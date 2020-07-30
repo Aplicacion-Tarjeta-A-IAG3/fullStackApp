9cc2cb447d8427d34e431f2e2f262990145d5ff3
Julia Espinoza <juespinozacorrea@gmail.com>
2020-07-29 22:28:00 -0300

    Update in transaction pages y Client dashboard (#24)
    * Transacciones: removed ids, refactor validations, added translations, add currency
    * Admin: refactor clients validations
    * Added Client dashboard with balance

--- src/models/resourcesMessages.js
+++ src/models/resourcesMessages.js
@@ -13,6 +13,7 @@ const resourceMessages = {
     personas: {
       name: "Clientes",
       fields: {
+        dni: "DNI",
         contacto: {
           mail: "Email",
         },
@@ -33,6 +34,10 @@ const resourceMessages = {
         cuit: "CUIT del comercio",
         tarjeta: "Nro. de tarjeta",
         cvc: "CVC",
+        tipo: "Tipo de transacción",
+        cliente: "Nombre del cliente",
+        comercio: "Nombre del comercio",
+        monto: "Monto",
       },
     },
   },
@@ -54,5 +59,10 @@ const resourceMessages = {
     "Respuesta incorrecta del servidor. Consulte al proveedor del servicio.",
   "Bad request":
     "¡Ha ocurrido un error! No se guardaron los cambios. [Respuesta del servidor: 'Bad Request']",
+  emptyValidation: "No puede estar vacío",
+  numericValidation: "Debe ser numérico",
+  emailValidation: "Email inválido",
+  "El formulario no es válido. Por favor verifique si hay errores":
+    "Datos incorrectos o incompletos. Por favor verifique el formulario y las pestañas",
 };
 export default resourceMessages;
