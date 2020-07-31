a961613dc574ec66a131ffdbdc01473823683560
Julia Espinoza <juespinozacorrea@gmail.com>
2020-07-31 16:48:59 -0300

    Client and Business dashboard fix (#28)
    * Fix all the console error in the clients' page and added profile page for clients
    * Add panel tabs for profile and previous balance
    * Fix business tabs and requests

--- src/components/bussiness/businessMonthlyBalance.js
+++ src/components/bussiness/businessMonthlyBalance.js
@@ -33,12 +33,25 @@ const useStyles = makeStyles((theme) => ({
   demo: {
     backgroundColor: theme.palette.background.paper,
   },
+  list: { primaryItem: "#000000" },
+  form: { backgroundColor: "#f4f4f0", padding: "2em" },
+  container: { minHeight: "400px", padding: "1em" },
+  info: {
+    backgroundColor: "#455A64",
+    marginTop: "1em",
+  },
+  header: {
+    backgroundColor: "#455A64",
+    color: "#fff",
+    marginTop: "1em",
+  },
 }));
 
 const columns = ["Monto (AR$)", "Tipo", "Detalle", "Fecha"];
 const username = localStorage.getItem("username");
 
 export default function BusinessMonthlyBalance(props) {
+  const { children, value, index, ...other } = props;
   const classes = useStyles();
   const [resumen, setResumen] = React.useState({
     isBalance: false,
@@ -68,7 +81,7 @@ export default function BusinessMonthlyBalance(props) {
       const result = await fetch(url, requestOptions);
       console.log("status", result.status);
       const dataResult = await result.json();
-      if (result.status === 200 && result.length > 0) {
+      if (result.status === 200) {
         const {
           esResumen,
           resumenDelMes,
@@ -84,14 +97,16 @@ export default function BusinessMonthlyBalance(props) {
           grossTotal: currencyParser(totalSinComisiones),
           fees: currencyParser(totalComisiones),
         });
-        setRows(
-          pagos.map(({ monto, tipoTransaccion, detalle, fecha }) => [
-            monto,
-            tipoTransaccion,
-            detalle,
-            fecha,
-          ])
-        );
+        if (isDefined(pagos)) {
+          setRows(
+            pagos.map(({ monto, tipoTransaccion, detalle, fecha }) => [
+              monto,
+              tipoTransaccion,
+              detalle,
+              fecha,
+            ])
+          );
+        }
         // console.log("business data:", dataResult);
         // console.log("business pagos:", dataResult.pagos);
       } else {
@@ -108,55 +123,57 @@ export default function BusinessMonthlyBalance(props) {
   }, []);
 
   return (
-    <Container fullwidth>
-      <Card>
-        <CardHeader
-          title="Detalles de tu resumen"
-          style={{ backgroundColor: "#455A64", color: "#fff" }}
-        />
-        <CardContent>
-          <div className={classes.demo}>
-            <List style={{ display: "flex" }}>
-              <ListItem>
-                <ListItemIcon>
-                  <TodayIcon />
-                </ListItemIcon>
-                <ListItemText
-                  secondary="Mes"
-                  primary={monthsMapper[resumen.month]}
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemIcon>
-                  <LibraryBooksIcon />
-                </ListItemIcon>
-                <ListItemText
-                  secondary="Total de transacciones"
-                  primary={resumen.grossTotal}
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemIcon>
-                  <Rotate90DegreesCcwIcon />
-                </ListItemIcon>
-                <ListItemText
-                  secondary="Total de comisiones"
-                  primary={resumen.fees}
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemIcon>
-                  <MoneyIcon />
-                </ListItemIcon>
-                <ListItemText
-                  secondary="Total a recibir"
-                  primary={resumen.netTotal}
-                />
-              </ListItem>
-            </List>
-          </div>
-        </CardContent>
-      </Card>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`client-tab-${index}`}
+      aria-labelledby={`client-tab-${index}`}
+      className={classes.container}
+      {...other}
+    >
+      <div className={classes.info}>
+        <CardHeader title="Detalles de tu resumen" className={classes.header} />
+        <div className={classes.demo}>
+          <List style={{ display: "flex" }}>
+            <ListItem>
+              <ListItemIcon>
+                <TodayIcon />
+              </ListItemIcon>
+              <ListItemText
+                secondary="Mes"
+                primary={monthsMapper[resumen.month]}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <LibraryBooksIcon />
+              </ListItemIcon>
+              <ListItemText
+                secondary="Total de transacciones"
+                primary={resumen.grossTotal}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <Rotate90DegreesCcwIcon />
+              </ListItemIcon>
+              <ListItemText
+                secondary="Total de comisiones"
+                primary={resumen.fees}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <MoneyIcon />
+              </ListItemIcon>
+              <ListItemText
+                secondary="Total a recibir"
+                primary={resumen.netTotal}
+              />
+            </ListItem>
+          </List>
+        </div>
+      </div>
       <Divider />
       <br />
       <MUIDataTable
@@ -165,6 +182,6 @@ export default function BusinessMonthlyBalance(props) {
         columns={columns}
         options={balanceTableOptions}
       />
-    </Container>
+    </div>
   );
 }
---
+++ src/components/clients/clientProfile.js
@@ -0,0 +1,234 @@
+import * as React from "react";
+import {
+  CardHeader,
+  Divider,
+  makeStyles,
+  Snackbar,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+} from "@material-ui/core";
+import { PasswordInput, SimpleForm, minLength, required } from "react-admin";
+import MuiAlert from "@material-ui/lab/Alert";
+import { isDefined } from "../../utils/helpers";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    maxWidth: "360px",
+    backgroundColor: theme.palette.background.paper,
+  },
+  demo: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  formControl: {
+    margin: "1em",
+  },
+  list: { primaryItem: "#000000" },
+  form: { backgroundColor: "#f4f4f0", padding: "2em" },
+  container: { minHeight: "400px", padding: "1em" },
+  info: {
+    backgroundColor: "#455A64",
+    marginTop: "1em",
+  },
+  header: {
+    backgroundColor: "#455A64",
+    color: "#fff",
+    marginTop: "1em",
+  },
+}));
+
+const username = localStorage.getItem("username");
+
+export default function ClientProfile(props) {
+  const { children, value, index, ...other } = props;
+  const classes = useStyles();
+  const [client, setClient] = React.useState(null);
+  const [oldPass, setOldPass] = React.useState(null);
+  const [newPass, setNewPass] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    const apiUrl = "https://african-express.us-e2.cloudhub.io/api/core";
+
+    const headers = {
+      "Content-Type": "application/json",
+      client_id: localStorage.getItem("clientId"),
+      client_secret: localStorage.getItem("clientSecret"),
+    };
+
+    const requestOptions = {
+      method: "GET",
+      headers: headers,
+    };
+
+    const getCLientData = async () => {
+      const url = `${apiUrl}/personas?dni=${username}`;
+
+      const result = await fetch(url, requestOptions);
+      console.log("response status", result.status);
+      if (result.status === 200) {
+        const dataResult = await result.json();
+        console.log("CLIENTe? status", dataResult);
+        setClient(dataResult[0]);
+      } else {
+        console.error("Ha ocurrido un error al recuperar los datos.");
+      }
+    };
+
+    getCLientData();
+  }, []);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const handleOldPassChange = (event) => {
+    const pass = event.target.value;
+    setOldPass(pass);
+  };
+
+  const handleNewPassChange = (event) => {
+    const pass = event.target.value;
+    setNewPass(pass);
+  };
+
+  const apiUrl = "https://african-express.us-e2.cloudhub.io/api/core";
+
+  const headers = {
+    "Content-Type": "application/json",
+    client_id: localStorage.getItem("clientId"),
+    client_secret: localStorage.getItem("clientSecret"),
+  };
+
+  const handleUpdatePass = () => {
+    const updatedClient = client;
+    updatedClient.password = newPass;
+    const url = `${apiUrl}/personas`;
+    const requestOptions = {
+      method: "PUT",
+      headers: headers,
+      body: JSON.stringify(updatedClient),
+    };
+    setClient(updatedClient);
+    fetch(url, requestOptions)
+      .then((response) => {
+        try {
+          let json = response.json();
+          // console.log("JSON??", json);
+          if (response.status >= 200 && response.status < 300) {
+            console.error(response);
+          } else {
+            // console.log("llega?", json);
+            return json;
+          }
+        } catch (err) {
+          console.error(err);
+        }
+      })
+      .then((result) => {
+        setNewPass(null);
+        setOldPass(null);
+        handleClick(null);
+      })
+      .catch((e) => {
+        console.error(e.message);
+      });
+  };
+
+  // !! this is necessary to validate values' lengths for NumberInput components
+  const validateUpdate = () => {
+    const errors = {};
+    if (isDefined(client) && client.password !== oldPass) {
+      errors.oldPass = ["passNotMatch"];
+    }
+    if (isDefined(client) && client.password === newPass) {
+      errors.newPass = ["sameNewPass"];
+    }
+    return errors;
+  };
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`client-tab-${index}`}
+      aria-labelledby={`client-tab-${index}`}
+      className={classes.container}
+      {...other}
+    >
+      <div className={classes.info}>
+        <CardHeader title="Mi datos" className={classes.header} />
+        <div className={classes.demo}>
+          {client && (
+            <List style={{ display: "flex" }}>
+              <ListItem>
+                <ListItemIcon>{/* <InboxIcon /> */}</ListItemIcon>
+                <ListItemText
+                  primary={`${client.nombre} ${client.apellido}`}
+                  secondary="Nombre y apellido"
+                />
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>{/* <InboxIcon /> */}</ListItemIcon>
+                <ListItemText
+                  primary={client.contacto.email}
+                  secondary="Email"
+                />
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>{/* <InboxIcon /> */}</ListItemIcon>
+                <ListItemText
+                  primary={`${client.puntos}`}
+                  secondary="Puntos acumulados"
+                />
+              </ListItem>
+            </List>
+          )}
+        </div>
+      </div>
+      <Divider />
+      <br />
+      <div className={classes.form}>
+        <SimpleForm save={handleUpdatePass} validate={validateUpdate}>
+          <PasswordInput
+            label="Contraseña actual"
+            source="oldPass"
+            required
+            fullWidth
+            onChange={handleOldPassChange}
+            validate={required("Campo obligatorio")}
+          />
+          <PasswordInput
+            label="Contraseña nueva"
+            source="newPass"
+            required
+            fullWidth
+            onChange={handleNewPassChange}
+            validate={[required("Campo obligatorio"), minLength(4)]}
+          />
+        </SimpleForm>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="success">
+            ¡Contraseña actualizada correctamente!
+          </Alert>
+        </Snackbar>
+      </div>
+      <Divider />
+      <br />
+    </div>
+  );
+}
