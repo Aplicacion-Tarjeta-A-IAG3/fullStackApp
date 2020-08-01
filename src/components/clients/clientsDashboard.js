1b8f43053eef61b18ba6fc75d7d4463b71f2e2b4
Julia Espinoza <juespinozacorrea@gmail.com>
2020-08-01 07:24:58 -0300

    update clients tab icons

--- src/components/clients/clientsDashboard.js
+++ src/components/clients/clientsDashboard.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Tabs, Tab } from "@material-ui/core";
 import MoneyIcon from "@material-ui/icons/Money";
 import GetAppIcon from "@material-ui/icons/GetApp";
+import CreditCardIcon from "@material-ui/icons/CreditCard";
 import PersonPinIcon from "@material-ui/icons/PersonPin";
 import ClientMonthlyBalance from "./clientMonthlyBalance";
 import ClientDailyBalance from "./clientDailyBalance";
@@ -41,8 +42,12 @@ export default function ClientsDashboard() {
           icon={<MoneyIcon />}
           id="client-tab-0"
         />
-        <Tab label="Resumen del mes" icon={<MoneyIcon />} id="client-tab-1" />
-        <Tab label="Pagar tarjetas" icon={<GetAppIcon />} id="client-tab-2" />
+        <Tab label="Resumen del mes" icon={<GetAppIcon />} id="client-tab-1" />
+        <Tab
+          label="Pagar tarjetas"
+          icon={<CreditCardIcon />}
+          id="client-tab-2"
+        />
         <Tab label="Mi Perfil" icon={<PersonPinIcon />} id="client-tab-3" />
       </Tabs>
       <ClientDailyBalance
