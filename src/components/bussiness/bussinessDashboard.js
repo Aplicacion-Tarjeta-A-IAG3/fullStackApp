a9df83872459aecb6991ff7411c79e634ce3bdb9
Julia Espinoza <juespinozacorrea@gmail.com>
2020-08-01 00:47:58 -0300

    Pay card on client dashboard (#31)

--- src/components/bussiness/bussinessDashboard.js
+++ src/components/bussiness/bussinessDashboard.js
@@ -1,47 +1,13 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
-import { Paper, Tabs, Tab, Typography, Box } from "@material-ui/core";
+import { Paper, Tabs, Tab } from "@material-ui/core";
 import MoneyIcon from "@material-ui/icons/Money";
 import GetAppIcon from "@material-ui/icons/GetApp";
 import PersonPinIcon from "@material-ui/icons/PersonPin";
-import BusinessDailyBalance from "./bussiness/businessDailyBalance";
-import BusinessMonthlyBalance from "./bussiness/businessMonthlyBalance";
-import BusinessProfile from "./bussiness/businessProfile";
-import BusinessPreviousBalances from "./bussiness/previousBalances";
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`scrollable-force-tabpanel-${index}`}
-      aria-labelledby={`scrollable-force-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `scrollable-force-tab-${index}`,
-    "aria-controls": `scrollable-force-tabpanel-${index}`,
-  };
-}
+import BusinessDailyBalance from "./businessDailyBalance";
+import BusinessMonthlyBalance from "./businessMonthlyBalance";
+import BusinessProfile from "./businessProfile";
+import BusinessPreviousBalances from "./previousBalances";
 
 const useStyles = makeStyles((theme) => ({
   root: {
